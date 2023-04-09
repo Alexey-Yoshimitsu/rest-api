0b3b2ba8adfde45095f2268751f08dfa3c9728f4
Alexey-Yoshimitsu <shandov466@gmail.com>
2023-04-09 19:21:16 +0300

    .

--- index.js
+++ index.js
@@ -1,64 +1,80 @@
 const express  = require("express")
 const app = express()
 const { v4: uuidv4 } = require('uuid');
-const mysql = require('mysql2');
+//const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'data'
-});
+
+
+//const connection = async (query)=>{
+//  let conn = await  mysql.createConnection({
+//      host: 'localhost',
+//      user: 'root',
+//      database: 'data'
+//    })
+//    
+//    
+//  return conn.execute(query)
+//}
+
 
 // simple query
-const CreateInsertQuery = (id, email, password, token)=>{
-query = "INSERT INTO `users` (`id`, `email`, `password`, `token`) VALUES ('"+id+"', '"+email+"', '"+password+"', '"+token+"');"
-connection.query(query,
-  function(err, results, fields) {
-    console.log(err)
-    console.log(results); // results contains rows returned by server
-    //console.log(fields); // fields contains extra meta data about results, if available
-  }
-);
-}
-const CreateSelectQuery = (query, count)=>{
-    queryRun = "SELECT `id`, `email`, `password`, `token` FROM `users`"
-    connection.query(queryRun,
-      function(err, results, fields) {
-        console.log(err)
-        console.log(results); // results contains rows returned by server
-        //console.log(fields); // fields contains extra meta data about results, if available
-      }
-    );
-    }
+//query = "INSERT INTO `users` (`id`, `
+//queryRun = "SELECT `id`, `email`, `password`, `token` FROM `users`"
 
-const CreateUpdateQuery = (newtoken,id)=>{
-        queryRun = "UPDATE `users` SET token = '"+newtoken+"' WHERE `id` = "+id+";"
-        connection.query(queryRun,
-          function(err, results, fields) {
-            console.log(err)
-            console.log(results); // results contains rows returned by server
-            //console.log(fields); // fields contains extra meta data about results, if available
-          }
-        );
-        }
+//queryRun = "UPDATE `users` SET token = '"+newtoken+"', "+auth+" WHERE `id` = "+id+";"
+//email`, `password`, `token`) VALUES ('"+id+"', '"+email+"', '"+password+"', '"+token+"');"
     
+
+//queryRun = "UPDATE `users` SET token = '"+newtoken+"', "+auth+" WHERE `id` = "+id+";"
+
+//async function connectionClose (){
+//  await connection.close()
+//}
+  
+//await connection.query("SELECT `id`, `email`, `password`, `token` FROM `users` WHERE `email`= "+email+", `password`="+password,
 
 const cookieParser = require('cookie-parser')
 // use jsons to parse body
 app.use(express.json())
 app.use(cookieParser());
 
-
 app.get("/api",(req,res)=>{
     
-    res.cookie('token', '09884266-5cfc-4363-8959-73e84da144af', { expires: new Date(Date.now() + 900000), httpOnly: true })
-    const result = CreateSelectQuery(count=1)
+    //res.cookie('token', '09884266-5cfc-4363-8959-73e84da144af', { expires: new Date(Date.now() + 900000), httpOnly: true })
+    //const result = CreateUpdateQuery(uuidv4(),1)
+    //let cookie = res.cookies;
+    //console.log(cookie);
     //console.log(result[0])
-    //res.cookie(``,`encrypted cookie string Value`);
-    res.json({"status":"200"})
+    if(res.cookie.token == undefined && {}){
+      res.redirect("/api-auth")
+    }
+    else{
+      res.json({"status":200})
+    }
+    //console.log(req.cookies.token);
     
 })
+app.get("/api-auth",(req,res)=>{
+    
+  //res.cookie('token', '09884266-5cfc-4363-8959-73e84da144af', { expires: new Date(Date.now() + 900000), httpOnly: true })
+  //const result = CreateSelectQuery(count=1)
+  //let cookie = res.cookies;
+  let answer ={
+    id:1,
+    
+    email
+  }
 
+
+  let email = req.body.email
+  let password = req.body.password
+  //let answer = connection('SELECT `id`, `email`, `password`, `token` FROM `users` WHERE `email`="shandov466@gmail.com"')
+
+
+  //if(req.body.email == )
+  res.json({email:email, password: password})
+  
+})
 
 app.listen(3000, () => {
     console.log(`Example app listening on port 228`)
